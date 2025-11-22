6e36478eb946eb68eeabcab61c00ba1818478a4f
Akshata Deole <deoleakshata@gmail.com>
2025-11-23 00:10:28 +0530

    Assignment 29 solution(Q1-Q5)

---
+++ Assignments/Assignment_29/program29_3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+void pattern(int iRow, int iCol)
+{
+   int i = 0, j = 0;
+   char ch ='a';
+   
+   
+
+   for(i=1;i<iRow;i++)
+   {
+    for(j=1;j<iCol;j++)
+    {
+        if(i % 2 == 0)
+        {
+            printf("%d\t",j);
+            
+        }
+        else
+        {
+            printf("%c\t",ch);
+            ch++;
+        }
+       
+    }
+     printf("\n");
+   }
+}
+int main()
+{
+    int iValue1 = 0, iValue2;
+
+    printf("Enter numbers of rows :");
+    scanf("%d",&iValue1);
+
+    printf("Enter numbers of columns :");
+    scanf("%d",&iValue2);
+
+    pattern(iValue1, iValue2);
+
+    return 0;
+}
