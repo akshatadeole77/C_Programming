1d20fedbad9bc718ffcacf43201ed21bd3743d53
Akshata Deole <deoleakshata@gmail.com>
2025-11-23 21:15:35 +0530

    Add files via upload

---
+++ Assignments/Assignment_31/program31_2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void pattern(int iRow, int iCol)
+{
+   int i = 0, j = 0;
+
+   for(i=1;i<=iRow;i++)
+   {
+    for(j=iCol;j >= i;j--)
+    {
+        printf("*\t");
+    }
+     printf("\n");
+   }
+}
+int main()
+{
+    int iValue1 = 0, iValue2;
+
+    printf("Enter numbers of rows :");
+    scanf("%d",&iValue1);
+
+    printf("Enter numbers of columns :");
+    scanf("%d",&iValue2);
+
+    pattern(iValue1, iValue2);
+
+    return 0;
+}
